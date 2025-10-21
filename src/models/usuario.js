d356fc2c2efa48a07bdaa8fa060f3b1efd324911
LeeroyRivera <leeroya506@gmail.com>
2025-10-20 23:40:30 -0600

    Modelo usuario creado

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,3 +1,32 @@
 // Modelo: Usuario
 const { DataTypes, Model } = require('sequelize');
 const db = require('../config/database');
+
+const Usuario = db.define('Usuario' , { 
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  contraseña: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  rol: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  fecha_creacion: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'usuarios', 
+  timestamps: false
+});
+
+module.exports = Usuario;
