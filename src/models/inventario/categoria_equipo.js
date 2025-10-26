2c754464f5cb0a7c1790bc41226b5f81cbfcfdde
allanpaz <apaz04028@gmail.com>
2025-10-26 16:46:52 -0600

    modelos categorias y equipos

--- src/models/inventario/categoria_equipo.js
+++ src/models/inventario/categoria_equipo.js
@@ -1,7 +1,23 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../config/database');
-
+const moment = require('moment');
 const CategoriaEquipo = sequelize.define('CategoriaEquipo', {}, {
+nombre_categoria: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  descripcion: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  fecha_creacion: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW //en caso de que dejes la fecha actual
+    //defaultValue: moment().format('YYYY-MM-DD HH:mm:ss')
+  }
+}, {
+
   tableName: 'categorias_equipo',
   timestamps: false
 });
