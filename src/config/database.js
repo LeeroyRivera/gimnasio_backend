e7a51da39e198d466b14e6d820b2880f7ffdaf98
allanpaz <apaz04028@gmail.com>
2025-11-04 14:22:24 -0600

    este tube que subirlo para cambiar de postgres a mysql

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,22 @@
 const sequelize = require('sequelize');
+const {USUARIO_DB, CONTRASENA_DB, NOMBRE_DB} = process.env;
+const db =  new sequelize(
+    NOMBRE_DB,
+    USUARIO_DB,
+    CONTRASENA_DB,
+    {
+        host: 'localhost',
+        port: 3306,
+        dialect: 'mysql'
+    }
+    );
+
+    module.exports = db;
+
+/*const sequelize = require('sequelize');
 
 const db = new sequelize.Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres', 
   protocol: 'postgres',
 });
-module.exports = db;
+module.exports = db;*/
