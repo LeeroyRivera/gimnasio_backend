e0d576caca3d6f9e349b525fae65ff77bed55f77
LeeroyRivera <leeroya506@gmail.com>
2025-11-12 00:51:11 -0600

    Refactorizado guardarUsuario()

--- src/controllers/usuarios/controladorUsuario.js
+++ src/controllers/usuarios/controladorUsuario.js
@@ -77,7 +77,7 @@ exports.guardarUsuario = async (req, res) => {
     }
     if (existeUsername) {
       return res
-     .status(409)
+        .status(409)
         .json({ error: "El nombre de usuario ya está en uso" });
     }
 
@@ -105,15 +105,13 @@ exports.guardarUsuario = async (req, res) => {
       );
 
       await t.commit();
-      
-      (async () => {
+      // Notificar registro
       try {
         await notificarRegistroUsuario(nuevoUsuario, nuevoCliente);
       } catch (e) {
         console.error("No se pudo enviar correo de registro:", e?.message || e);
       }
-    })();
-      
+
       return res
         .status(201)
         .json({ usuario: nuevoUsuario, cliente: nuevoCliente });
